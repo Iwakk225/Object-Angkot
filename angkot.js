1186ef15e6cc8b1e2829e2e8df850a40392c47d8
Iwakk225 <muhammadadityarahmansyah18@gmail.com>
2025-06-03 10:58:54 +0700

    menambahkan function jika ada penumpang naik

--- angkot.js
+++ angkot.js
@@ -1,8 +1,17 @@
 function Angkot(sopir, trayek, penumpang, kas) {
-    this.sopir = sopir;
-    this.trayek = trayek; 
-    this.penumpang = penumpang;
-    this.kas = kas;
+  this.sopir = sopir;
+  this.trayek = trayek;
+  this.penumpang = penumpang;
+  this.kas = kas;
+
+//   jika ada penumpang naik
+  this.penumpangNaik = function (namaPenumpang) {
+    this.penumpang.push(namaPenumpang);
+    return this.penumpang;
+  };
+
+
+
 }
 
 var angkot1 = new Angkot("Adit", ["Sawahan", "Tunjungan"], [], 0);
