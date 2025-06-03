858963275f97cba9d774111aa5c6bf0272842a33
Iwakk225 <muhammadadityarahmansyah18@gmail.com>
2025-06-03 11:13:41 +0700

    menambahkan function dan perulangan untuk penumpang yang turun dan bayar

--- angkot.js
+++ angkot.js
@@ -4,15 +4,32 @@ function Angkot(sopir, trayek, penumpang, kas) {
   this.penumpang = penumpang;
   this.kas = kas;
 
-//   jika ada penumpang naik
+  //   jika ada penumpang naik
   this.penumpangNaik = function (namaPenumpang) {
     // push namaPenumpang ke array penumpang
     this.penumpang.push(namaPenumpang);
     return this.penumpang;
   };
 
-
-
+//   jika ada penumpang turun
+// fungsi ini menerima namaPenumpang dan bayar
+  this.penumpangTurun = function (namaPenumpang, bayar) {
+    // jika penumpang tidak ada di dalam angkot
+    if (this.penumpang.length === 0) {
+        // tampilkan alert
+      alert("Angkot masih kosong!");
+      return false;
+    }
+    // jika penumpang ada di dalam angkot
+    for (var i = 0; i < this.penumpang.length; i++) {
+        // jika namaPenumpang ditemukan di dalam array penumpang
+      if (this.penumpang[i] === namaPenumpang) {
+        this.penumpang[i] = undefined; // menghapus penumpang yang turun
+        this.kas += bayar; // menambah kas
+        return this.penumpang; // mengembalikan array penumpang
+      }
+    }
+  };
 }
 
 var angkot1 = new Angkot("Adit", ["Sawahan", "Tunjungan"], [], 0);
